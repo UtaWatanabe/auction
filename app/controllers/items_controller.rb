30c56da08795e0df04ea331347015298de0af8bd
yt.watanabe <yt.w.sannta@gmail.com>
2016-02-14 18:42:27 +0900

    new / update 周りの業務完了｡ 全体の流れを理解しているものの､汎用性にかけそう｡要復習｡

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,39 @@ class ItemsController < ApplicationController
 def index
     @item = Item.all
 end
+  def new
+    @item = Item.new
+  end
 
   def show
     @item = Item.find(params[:id])
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+end
+
+  private
+  def item_params
+    params.require(:item).permit(
+        :name, :description, :price,
+        :email, :seller_id, :image_url
+      )
+
+  end
+
+
 end
 
